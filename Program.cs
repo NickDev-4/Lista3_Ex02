6054601c4964075a367b089e704f16a1820c0572
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-18 10:50:34 -0300

    Exercicio02

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+double salario, total;
+
+Console.Write("Digite o seu salário: ");
+salario = double.Parse(Console.ReadLine());
+
+total = salario * 0.08;
+
+Console.WriteLine("");
+
+Console.WriteLine("Salário (R$)..: {0}", salario);
+
+Console.WriteLine("FGTS: R$ {0:f2}", total);
